e19977c0ca8ed22903c284d0855548e6e8598047
jack123abc4 <jack.r.motherway@gmail.com>
2022-08-25 12:35:43 -0400

    event blocks are text fields

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,30 +4,37 @@ calendarEl.textContent = moment().format("dddd, MMM Do");
 
 
 // create rows
-// var rowEl = document.querySelector("row")
 var containerEl = document.querySelector(".container")
-for (var i = 0; i < 8; i++) {
+for (var i = 0; i < 9; i++) {
     // add row to container
     var rowEl = document.createElement("div")
     rowEl.classList.add("row");
     containerEl.appendChild(rowEl);
 
-    // add timeblocks to row
+    // add hour block to row
     var hourColEl = document.createElement("div");
     hourColEl.classList.add("hour", "time-block", "col-1");
     var hourBlock = moment({hour:9+i, minute:0})
     hourColEl.textContent = hourBlock.format("hh:mm");
     rowEl.appendChild(hourColEl);
 
-    var timeblockColEl = document.createElement("div");
+    // add event block to row
+    var timeblockColEl = document.createElement("input");
     timeblockColEl.classList.add("description", "time-block", "col-10");
     rowEl.appendChild(timeblockColEl);
 
+    // add save block to row
     var saveColEl = document.createElement("div");
     saveColEl.classList.add("saveBtn", "time-block", "col-1");
     rowEl.appendChild(saveColEl);
 
+    // add save icon to save block
+    var saveColIconEl = document.createElement("i");
+    saveColIconEl.classList.add("fa-solid", "fa-floppy-disk");
+    saveColEl.appendChild(saveColIconEl);
+
     // set past, present, future styles
+    
     // var hourNow = moment();
     var hourNow = moment({hour:13, minute:0});
     console.log("Block: " + hourBlock.format("hh:mm") + "\n" + "Current time: " + hourNow.format("hh:mm"));
